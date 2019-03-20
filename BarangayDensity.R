b7f65dc0269c029da364fefd6683fb6e99ac08e0
TiptapTulips <jmsahagun@globe.com.ph>
2019-03-20 11:01:30 +0800

    Separation of city population density computation and barangay population density computation

--- BarangayDensity.R
+++ BarangayDensity.R
@@ -11,8 +11,6 @@ library(dplyr)
 # Count of Barangay per Region
 CountPerRegion <- group_by(population, Region)
 CountPerRegionSummary <-summarise(CountPerRegion, BPR = n())
-View(CountPerRegionSummary)
-
 
 #Population per Barangay
 populationByBarangay <- group_by(population, Barangay, Region,CityProvince)
@@ -29,31 +27,3 @@ FinalSorted = Final %>% arrange(desc(Density))
 Top5Barangay <- FinalSorted[1:5,]
 View(Top5Barangay)
 write.csv(FinalTop5, file = "Population Density by Barangay.csv")
-
-
-
-
-
-#FOR CITY
-library(dplyr)
-population <- read.csv("population.csv")
-regionarea <- read.csv("regionarea.csv")
-
-# Count of Cities per Region
-CountPerRegion <- group_by(population, regions = Region)
-CountPerRegionSummary <-summarise(CountPerRegion, Count = n())
-
-# Population Per City
-populationByCity <- group_by(population, CityProvince, Region)
-populationSummary <- arrange(summarise(populationByCity, PopulationPerCity = sum(Population)), regions = Region)
-
-CityArea <- regionarea$Area / CountPerRegionSummary$Count
-regions <- (regionarea[,1])
-dataRawFinal <- bind_cols(list(Region = regions, CityArea = CityArea))
-
-FinalTable <- inner_join(x = populationSummary, y = dataRawFinal, by = "Region")
-Final <- mutate(FinalTable, Density = PopulationPerCity/CityArea)
-FinalSorted = Final %>% arrange(desc(Density))
-FinalTop5City <- FinalSorted[1:5,]
-View(FinalTop5City)
-write.csv(FinalTop5, file = "Population Density by City.csv")
